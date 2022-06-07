0ab4f08415e526ab0844571802db3a85f17f3e2e
jibisk <jiby.elavinmootil@gmail.com>
2022-06-07 20:43:44 +0530

    adding cart count

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import { useContext } from "react";
 
-import Button from '../custom-button/custom-button.component';
+import Button from "../custom-button/custom-button.component";
 
-import './cart-dropdown.styles.scss';
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => (
-  <div className='cart-dropdown-container'>
-    <div className='cart-items' />
-    <Button>GO TO CHECKOUT</Button>
-  </div>
-);
+import { CartContext } from "../../contexts/cart.context";
 
+import "./cart-dropdown.styles.scss";
+
+const CartDropdown = () => {
+
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 export default CartDropdown;
